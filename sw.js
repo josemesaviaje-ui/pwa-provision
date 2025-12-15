ffb6c82051a6cc8ed4f1e871dc23ee3413f95190
josemesaviaje-ui <josemesaviaje@gmail.com>
2025-12-15 22:52:34 +0100

    Actualizacion migracion firebase

--- sw.js
+++ sw.js
@@ -1,48 +1,13 @@
-const CACHE_NAME = "provisiones-pwa-v1";
+// sw.js - Versión simple sin caché agresiva
 
-const FILES_TO_CACHE = [
-  "index.html",
-  "clientes.html",
-  "cliente.html",
-  "informes.html",
-
-  "manifest.json",
-
-  "css/main.css",
-
-  "js/storage.js",
-  "js/clientes.js",
-  "js/condiciones.js",
-  "js/dashboard.js",
-  "js/informes.js",
-  "js/exportExcel.js",
-  "js/exportPdf.js",
-  "js/navigation.js",
-  "js/theme.js"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
+self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
